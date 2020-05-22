ec8b915fbeb797f34f8970983ce3cc223a4fdb69
wellingtondantas <wellingtondantas.almeida@gmail.com>
2020-05-21 22:15:45 -0300

    My new site

---
+++ MySite/index.php
@@ -0,0 +1,328 @@
+<?php
+	$adddreslink = "#";
+	$adddresimg = "#";
+?>
+
+<!DOCTYPE HTML>
+<html lang="pt-br">
+   <head>
+      <title>WELLINGTON DANTAS - Data Scientist & Web Developer</title>
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta charset="UTF-8">
+      <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700%7CAllura" rel="stylesheet">
+      <link rel="shortcut icon" href="<?php echo $adddresimg; ?>ico.png" /> 
+     
+      <link href="<?php echo $adddreslink; ?>bootstrap.css" rel="stylesheet">
+      <link href="<?php echo $adddreslink; ?>ionicons.css" rel="stylesheet">
+      <link href="<?php echo $adddreslink; ?>fluidbox.min.css" rel="stylesheet">
+      <link href="<?php echo $adddreslink; ?>styles.css" rel="stylesheet">
+      <link href="<?php echo $adddreslink; ?>responsive.css" rel="stylesheet">
+      
+      
+      
+      
+      
+   </head>
+   <body>
+      <header>
+         <div class="container">
+            <div class="heading-wrapper">
+               <div class="row">
+                  <div class="col-sm-6 col-md-6 col-lg-4">
+                     <div class="info">
+                        <i class="icon ion-ios-location-outline"></i>
+                        <div class="right-area">
+                           <h5>R. Duarte Coelho, São Bento</h5>
+                           <h6>Fortaleza, CE - BRASIL</h6>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-sm-6 col-md-6 col-lg-4">
+                     <div class="info">
+                        <i class="icon ion-ios-telephone-outline"></i>
+                        <div class="right-area">
+                           <h5>+55 85 98508-7417</h5>
+                           <h6>CONTATO</h6>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-sm-6 col-md-6 col-lg-4">
+                     <div class="info">
+                        <i class="icon ion-ios-chatboxes-outline"></i>
+                        <div class="right-area">
+                           <h5><a href="https://www.linkedin.com/in/wellington-dantas-18084172/" style="color:#fff">wellingtondantas.almeida</a></h5>
+                           <h6>LINKEDIN</h6>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <a class="downlad-btn" href="<?php echo $adddreslink; ?>#">Download CV</a>
+         </div>
+      </header>
+      <section class="intro-section" style="background-image:url(<?php echo $adddresimg; ?>slider-1-1920x1080-min.jpg)">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-1 col-lg-2"></div>
+               <div class="col-md-10 col-lg-8">
+                  <div class="intro">
+                     <div class="profile-img"><img src="<?php echo $adddresimg; ?>wellington-dantas-de-almeida.jpg" alt="Wellington Dantas de Almeida"></div>
+                     <h2><b>Wellington Dantas</b></h2>
+                     <h4 class="font-yellow">Data Scientist & Web Developer</h4>
+                     <ul class="information margin-tb-30">
+                        <li><b>EMAIL : </b>wellingtondantas.almeida@gmail.com</li>
+                     </ul>
+                     <ul class="social-icons">
+                        <li><a href="https://www.linkedin.com/in/wellington-dantas-18084172/" target="_brank"><i class="ion-social-linkedin"></i></a></li>
+                        <li><a href="https://github.com/wellingtondantas" target="_brank"><i class="ion-social-github"></i></a></li>
+                        <li><a href="https://www.instagram.com/wellingtondantasa/" target="_brank"><i class="ion-social-instagram"></i></a></li>
+                        
+                     </ul>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      
+      
+      <section class="about-section section">
+         <div class="container">
+            <div class="row">
+               <div class="col-sm-4">
+                  <div class="heading">
+                     <h3><b>Quem sou</b></h3>
+                     <h6 class="font-lite-black"><b>PROFISSIONAL DEDICADO</b></h6>
+                  </div>
+               </div>
+               <div class="col-sm-8">
+                  <p class="margin-b-50">Olá, seja bem-vindo ao meu site. Meu nome é Wellington Dantas e sou natural de Fortaleza. 
+                  
+                  Sou apaixonado por tecnologias e trabalho com Desenvolvimento Web desde 2012. 
+                  Atualmente eu aproveito boa parte do meu tempo estudando Inteligência Artificial, nas áreas de Machine Learning e Deep Learning. 
+                  Também sou Mestrando de Inteligência Artificial com ênfase em Comunicações e sou graduado em Engenharia de Telecomunicações. 
+                  <b>Abaixo veja meu grau de interesse atualmente:</b>
+                  </p>
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  <div class="row">
+                     <div class="col-sm-6 col-md-6 col-lg-3">
+                        <div class="radial-prog-area margin-b-30">
+                           <div class="radial-progress" data-prog-percent="1.00">
+                              <div></div>
+                              <h6 class="progress-title">DATA SCIENCE</h6>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="col-sm-6 col-md-6 col-lg-3">
+                        <div class="radial-prog-area margin-b-30">
+                           <div class="radial-progress" data-prog-percent="1.00">
+                              <div></div>
+                              <h6 class="progress-title">WEB DEVELOPMENT</h6>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="col-sm-6 col-md-6 col-lg-3">
+                        <div class="radial-prog-area margin-b-30">
+                           <div class="radial-progress" data-prog-percent="1.00">
+                              <div></div>
+                              <h6 class="progress-title">VISUALIZAÇÃO DE DADOS</h6>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="col-sm-6 col-md-6 col-lg-3">
+                        <div class="radial-prog-area margin-b-30">
+                           <div class="radial-progress" data-prog-percent="1.00">
+                              <div></div>
+                              <h6 class="progress-title">TECNOLOGIA</h6>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      
+      
+      
+      <section class="experience-section section">
+         <div class="container">
+            <div class="row">
+               <div class="col-sm-4">
+                  <div class="heading">
+                     <h3><b>Experiência</b></h3>
+                     <h6 class="font-lite-black"><b>TRABALHOS PROFISSIONAIS</b></h6>
+                  </div>
+               </div>
+               <div class="col-sm-8">
+                  <div class="experience margin-b-50">
+                     <h4><b>PESQUISADOR DE INTELIGÊNCIA ARTIFICIAL</b></h4>
+                     <h5 class="font-yellow"><b>LABORATÓRIO DE INTELIGÊNCIA ARTIFICIAL</b></h5>
+                     <h6 class="margin-t-10">AGOSTO DE 2018 - PRESENTE</h6>
+                     <p class="font-semi-white margin-tb-30">Construir modelos de treinamento e predição com Machine Learning. Principalmente aplicando classificação com opção
+                     de rejeição para melhorar resultados de acurácia. Apresentar resultados de forma compreensível e com rigor científico.
+                     </p>
+                     <ul class="list margin-b-30">
+                        <li>Programação: Python, R e Matlab.</li>
+                        <li>Análise de dados e Visualização de dados.</li>
+                        <li>Aplicação em Algoritmos de Inteligência Artificial.</li>
+                        <li>Desenvolvimento de relatórios com rigor técnico.</li>
+                     </ul>
+                  </div>
+                  
+                  <div class="experience margin-b-50">
+                     <h4><b>FREELANCE : DATA SCIENCE / WEB DEVELOPER / CLOUD AWS </b></h4>
+                     <h5 class="font-yellow"><b>WELLINGTONDANTAS.COM.BR</b></h5>
+                     <h6 class="margin-t-10">JULHO DE 2012 - PRESENT</h6>
+                     <p class="font-semi-white margin-tb-30">Negócio próprio de ensino de Data Science e desenvolver Web Sites por encomenda. 
+                     </p>
+                     <ul class="list margin-b-30">
+                        <li>Ensino e Aplicação de Machine Learning.</li>
+                        <li>Visualização de dados com Power BI.</li>
+                        <li>Desenvolvimento de Sites, Blogs e Portais.</li>
+                        <li>Manutenção de Servidor de Hospedagem.</li>
+                        <li>Utilização de S3, EC2, ROUTE53, RDS da AWS.</li>
+                        <li>Linguagens: PHP, JS, HTML, CSS & WORDPRESS.</li>
+                     </ul>
+                  </div>
+                  
+                  
+                  
+                  <div class="experience margin-b-50">
+                     <h4><b>WEB MASTER/WEB DEVELOPER</b></h4>
+                     <h5 class="font-yellow"><b>FORÇA DIGITAL - TRUPPEONE</b></h5>
+                     <h6 class="margin-t-10">JULHO DE 2012 - JULHO DE 2015</h6>
+                     <p class="font-semi-white margin-tb-30">Agência especializada em comunicação. Uma agência que trabalha para dar soluções digitais para empresa nas áreas
+                     de criação e otimização de sites para mecanismos de busca, mídia sociais e marketing digital.
+                     </p>
+                     <ul class="list margin-b-30">
+                        <li>Desenvolvimento de Sites, Blogs e Portais.</li>
+                        <li>Manutenção de Servidor de Hospedagem.</li>
+                     </ul>
+                  </div>
+                  
+                  
+                  <div class="experience margin-b-50">
+                     <h4><b>OUTRAS EXPERIÊNCIAS</b></h4>
+                     <h5 class="font-yellow"><b>VÁRIOS LUGARES</b></h5>
+                     <h6 class="margin-t-10">VER ABAIXO</h6>
+                     <p class="font-semi-white margin-tb-30">Tive experiências como Estagiário de Engenharia, Professor de Informática, Aluno de Iniciação Científica e Adminstrador de Empresa Familiar.
+                     </p>
+                     <ul class="list margin-b-30">
+                        <li>Estagiário de Engenharia - TRE/CE - (1.3 anos)</li>
+                        <li>Professor de Informática - CENTEC - (0.5 anos).</li>
+                        <li>Aluno de Iniciação Científica - IFCE - (3 anos).</li>
+                        <li>Adminstrador de Empresa - TRANSFRETAR - (6 anos).</li>
+                     </ul>
+                  </div>
+                  
+                  
+               </div>
+            </div>
+         </div>
+      </section>
+      
+      
+      <section class="education-section section">
+         <div class="container">
+            <div class="row">
+               <div class="col-sm-4">
+                  <div class="heading">
+                     <h3><b>Educação</b></h3>
+                     <h6 class="font-lite-black"><b>CARREIRA ACADÊMICA</b></h6>
+                  </div>
+               </div>
+               <div class="col-sm-8">
+                  <div class="education-wrapper">
+                     <div class="education margin-b-50">
+                        <h4><b>MESTRADO EM INTELIGÊNCIA ARTIFICIAL</b></h4>
+                        <h5 class="font-yellow"><b>IFCE - INSTITUTO FEDERAL DO CEARÁ</b></h5>
+                        <h6 class="font-lite-black margin-t-10">PREVISÃO DE TÉRMINO EM AGOSTO DE 2020 (2 ANOS)</h6>
+                        <p class="margin-tb-30">O mestrado acadêmico em engenharia de telecomunicações com área de inteligência artificial aplicado a comunicações de dados
+                        apresenta conceitos e técnicas para formar profissionais qualificados para as mais diversas áreas do conhecimento, seja para 
+                        o mercado de trabalho como para a área de pesquisa científica.
+                        </p>
+                     </div>
+                     <div class="education margin-b-50">
+                        <h4><b>GRADUAÇÃO EM ENGENHARIA DE TELECOMUNICAÇÕES</b></h4>
+                        <h5 class="font-yellow"><b>IFCE - INSTITUTO FEDERAL DO CEARÁ</b></h5>
+                        <h6 class="font-lite-black margin-t-10">GRADUADO EM AGOSTO DE 2018 (5 ANOS)</h6>
+                        <p class="margin-tb-30">O Curso de Engenharia de Telecomunicações incorpora as novas tecnologias digitais que unem as áreas de telecomunicações e 
+                        computação. O curso se propõe a preparar profissionais para o planejamento de sistemas digitais e analógicos, sistemas ópticos, 
+                        sistemas satélite e de microondas, sistemas de telemedicina, sistemas de teleducação, sistemas de computação, dentre outros, além de prover um 
+                        ambiente propício ao empreendedorismo e à pesquisa e inovação através das incubadoras de empresas e de seus institutos de pesquisa.
+                        </p>
+                     </div>
+                     <div class="education margin-b-50">
+                        <h4><b>TÉCNICO PROFISSONAL EM INFORMÁTICA</b></h4>
+                        <h5 class="font-yellow"><b>ESCOLA PROFISSIONAL</b></h5>
+                        <h6 class="font-lite-black margin-t-10">TÉCNICO EM MAIO DE 2013 (4 ANOS)</h6>
+                        <p class="margin-tb-30">O curso técnico em informática oferecido por está instituição é voltado para a tecnologia da informação, 
+                        com mais ênfase em desenvolvimento de programas e redes de computadores. Além disso, introduz uma metodologia de conhecimento em 
+                        outras áreas, como: suporte a computadores, administração de bancos de dados e gestão de informática. Também, tem o objetivo de 
+                        formar profissionais comprometidos, éticos e inovadores.
+                        </p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      
+      <section class="counter-section" id="counter" style="background:#FFAD01;">
+         <div class="container">
+            <div class="row">
+               <div class="col-sm-6 col-md-6 col-lg-3">
+                  <div class="counter margin-b-30">
+                     <h1 class="title"><b><span class="counter-value" data-duration="400" data-count="2.5">0</span></b></h1>
+                     <h5 class="desc"><b>Formações Concluídas</b></h5>
+                  </div>
+               </div>
+               <div class="col-sm-6 col-md-6 col-lg-3">
+                  <div class="counter margin-b-30">
+                     <h1 class="title"><b><span class="counter-value" data-duration="1400" data-count="25">0</span></b></h1>
+                     <h5 class="desc"><b>Cursos Realizados</b></h5>
+                  </div>
+               </div>
+               <div class="col-sm-6 col-md-6 col-lg-3">
+                  <div class="counter margin-b-30">
+                     <h1 class="title"><b><span class="counter-value" data-duration="3000" data-count="400">0</span></b></h1>
+                     <h5 class="desc"><b>Horas Estudando Data Science</b></h5>
+                  </div>
+               </div>
+               <div class="col-sm-6 col-md-6 col-lg-3">
+                  <div class="counter margin-b-30">
+                     <h1 class="title"><b><span class="counter-value" data-duration="2000" data-count="60">0</span></b></h1>
+                     <h5 class="desc"><b>Projetos Web Concluídos</b></h5>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      
+      
+      <footer>
+         <p class="copyright">
+            Copyright &copy;All rights reserved | This template is made with <i class="ion-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+         </p>
+      </footer>
+      
+      <script src="<?php echo $adddreslink; ?>jquery-3.2.1.min.js" type="05e1f110999ac9dc93d4ef15-text/javascript"></script>
+      <script src="<?php echo $adddreslink; ?>tether.min.js" type="05e1f110999ac9dc93d4ef15-text/javascript"></script>
+      <script src="<?php echo $adddreslink; ?>bootstrap.js" type="05e1f110999ac9dc93d4ef15-text/javascript"></script>
+      <script src="<?php echo $adddreslink; ?>isotope.pkgd.min.js" type="05e1f110999ac9dc93d4ef15-text/javascript"></script>
+      <script src="<?php echo $adddreslink; ?>jquery.waypoints.min.js" type="05e1f110999ac9dc93d4ef15-text/javascript"></script>
+      <script src="<?php echo $adddreslink; ?>progressbar.min.js" type="05e1f110999ac9dc93d4ef15-text/javascript"></script>
+      <script src="<?php echo $adddreslink; ?>jquery.fluidbox.min.js" type="05e1f110999ac9dc93d4ef15-text/javascript"></script>
+      <script src="<?php echo $adddreslink; ?>scripts.js" type="05e1f110999ac9dc93d4ef15-text/javascript"></script>
+      
+      <script src="<?php echo $adddreslink; ?>rocket-loader.min.js" data-cf-settings="05e1f110999ac9dc93d4ef15-|49" defer=""></script>
+   </body>
+</html>

7c19c15393e8ff87d3842254aec189e57c722f28
Wellington Dantas <wellingtondantas.almeida@gmail.com>
2021-01-16 20:10:42 -0300

    MySite-Update02
    Atualização de comandos de WordPress

--- MySite/header.php
+++ MySite/header.php
@@ -23,3 +23,34 @@
         }
     ?>
 </title>
+
+<?php bloginfo('template_url'); ?>
+<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css">
+
+<?php bloginfo('description'); ?>
+<meta name="description" content="<?php bloginfo('description'); ?>"/>
+
+<?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+
+
+<?php wp_nav_menu(array('menu' => 'Nome do Menu', 'container' => 'nav', 'container_class' => 'menu-header-menu-container','menu_class' => 'menu-header-menu')); ?>
+
+exemplo:
+
+<div id="site-navigation" class="main-navigation">
+    <nav class="menu-header-menu-container">
+       <ul id="menu-menu-destaque" class="menu-header-menu">
+          <li id="menu-item-478" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-478">
+             <a>Página Inicial</a></li>
+
+
+
+
+
+
+
+
+
+
+
+
